63e67ba0dccc4a38e4aac90a92db2f6921316bb1
Miguel Tejera <miguel.tejera.45@gmail.com>
2022-03-15 15:16:08 +0900

    New features added

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from "react";
 import Board from "./components/Board";
 
 export default function App() {
-
   const [seconds, setSeconds] = React.useState(0);
   const [isActive, setIsActive] = React.useState(false);
-  const [totalWords, setTotalWords] = React.useState(null);
+  const [totalRightWords, setTotalRightWords] = React.useState(null);
   const [paragraphNum, setParagraphNum] = React.useState(0);
   const [wrongWordsArray, setWrongWordsArray] = React.useState([]);
   const [mistakesOn, setMistakesOn] = React.useState(false);
   const [levelSeleted, setLevelSeleted] = React.useState(true);
-  const [check, setCheck] = React.useState(false);
+  const [typeToStarOn, setTypeToStarOn] = React.useState(false);
+  const [text, setText] = React.useState("");
+  const [paragraph, setParagraph] = React.useState(["As you know, we have been working on the new perfume that we are launching in April and we are unsure about some of the packaging details. We have seen some of your creative work in the sales department and we think you have a very good eye for detail. \n Do you have some time before close of business this Friday to sit down with us and talk through some of our designs? We would truly appreciate your advice on this.", "Teenagers might have their parents to thank for their smartphone and social media addiction as their parents were the early adopters of the smartphone. Peter, 38 and father of two teenagers, reports that he used to be on his phone or laptop constantly. I was always connected and I felt like I was always working, he says. How could I tell my kids to get off their phones if I was always in front of a screen myself? So, in the evenings and at weekends, he takes his sim card out of his smartphone and puts it into an oldstyle mobile phone that can only make calls and send text messages.", "When Gabriela became aware of the cultural differences between her and her team, she took the initiative to have an open conversation with them about their feelings about her leadership. Pleased to be asked for their thoughts, Gabriela's team openly expressed that they were not used to being told what to do. They enjoyed having more room for initiative and creative freedom. When she told her team exactly what she needed them to do, they felt that she didn't trust them to do their job well. They realised that Gabriela was taking it personally when they tried to challenge or make changes to her decisions, and were able to explain that it was how they'd always worked. \n With a better understanding of the underlying reasons behind each other's behaviour, Gabriela and her team were able to adapt their way of working."])
 
-  const [paragraph, setParagraph] = React.useState(["As you know, we have been working on the new perfume that we are launching in April and we are unsure about some of the packaging details. We have seen some of your creative work in the sales department and we think you have a very good eye for detail. \n Do you have some time before close of business this Friday to sit down with us and talk through some of our designs? We would truly appreciate your advice on this.", "Teenagers might have their parents to thank for their smartphone and social media addiction as their parents were the early adopters of the smartphone. Peter, 38 and father of two teenagers, reports that he used to be on his phone or laptop constantly. I was always connected and I felt like I was always working, he says. How could I tell my kids to get off their phones if I was always in front of a screen myself? So, in the evenings and at weekends, he takes his sim card out of his smartphone and puts it into an oldstyle mobile phone that can only make calls and send text messages.", "When Gabriela became aware of the cultural differences between her and her team, she took the initiative to have an open conversation with them about their feelings about her leadership. Pleased to be asked for their thoughts, Gabriela's team openly expressed that they were not used to being told what to do. They enjoyed having more room for initiative and creative freedom. When she told her team exactly what she needed them to do, they felt that she didn't trust them to do their job well. They realised that Gabriela was taking it personally when they tried to challenge or make changes to her decisions, and were able to explain that it was how they'd always worked. \n With a better understanding of the underlying reasons behind each other's behaviour, Gabriela and her team were able to adapt their way of working."]
-  );
-  console.log("wrongWordsArray :" + wrongWordsArray)
-  console.log("isActive :" + isActive)
-  console.log("levelSeleted :" + levelSeleted)
-  console.log("paragraphNum :" + paragraphNum)
+  // console.log("wrongWordsArray :" + wrongWordsArray)
+  // console.log("rightWordsArray :" + rightWordsArray)
+  // console.log("isActive :" + isActive)
+  // console.log("levelSeleted :" + levelSeleted)
+  // console.log("paragraphNum :" + paragraphNum)
 
   function handleKeyPress(event){
     if(event.key === 'Enter'){
@@ -30,55 +30,52 @@ export default function App() {
     }
   }
   function level(event){
-    setTotalWords(null)
     setIsActive(false)
     setMistakesOn(false)
-    setWrongWordsArray([])
     setLevelSeleted(false)
+    setText("")
+    setWrongWordsArray([])
+    setTotalRightWords(null)
     setSeconds(event.target.value)
     setParagraphNum(event.target.name)
-    document.getElementById("textAreaId").value =""
     document.getElementById("textAreaId").focus()
     document.getElementById("textAreaId").setAttribute("class", "textAreaStyle_Enabled")
   }
-  function startGameByTyping(){
-    setMistakesOn(false)
+  function startGameByTyping(event){
+    const {value} = event.target
+    setText(value)
     setIsActive(true)
-    setCheck(true)
-    setTotalWords(null)
+    setTypeToStarOn(true)
   }
   function resetGame(){
-    setTotalWords(null)
     setSeconds(0)
-    setIsActive(false)
+    setText("")
+    setTotalRightWords(null)
     setParagraphNum(null)
-    setWrongWordsArray([])
+    setIsActive(false)
     setMistakesOn(false)
+    setTypeToStarOn(false)
     setLevelSeleted(true)
-    setCheck(false)
-    document.getElementById("textAreaId").value =""
+    setWrongWordsArray([])
     document.getElementById("textAreaId").setAttribute("class", "textAreaStyle_Disabled")
   }
-
-  let rightWords =[]
-
   function calculator(){
     setMistakesOn(true)
-    const wordsArr = document.getElementById("textAreaId").value.trim().split(" ")
-    let paragraphArray = paragraph[paragraphNum].split(" ")
+    const wordsArray = text.trim().split(" ")
+    const paragraphArray = paragraph[paragraphNum].split(" ")
 
-        for (let i = 0; i < wordsArr.length; i++){
-      if (paragraphArray[i] === wordsArr[i]){
-        rightWords.push(wordsArr[i])
-      } else {   
-        setWrongWordsArray(prevArray => [...prevArray, wordsArr[i]])
-      }
+      for (let i = 0; i < wordsArray.length; i++){
+        if (paragraphArray[i] === wordsArray[i]){
+          setTotalRightWords(prevState => prevState + 1)
+        } else {   
+          setWrongWordsArray(prevArray => [...prevArray, wordsArray[i]])
+        }
     }
-    return setTotalWords(rightWords.length)
   }
 
   React.useEffect(() => {
     let interval = null;
+    
     if (isActive) {
       interval = setInterval(() => {
         setSeconds(seconds => seconds - 1);
@@ -86,12 +83,12 @@ export default function App() {
     } 
     if (isActive && seconds === 0) {
       clearInterval(interval);
-        document.getElementById("textAreaId").setAttribute("class", "textAreaStyle_Block")
-        document.getElementById("textAreaId").blur()
-        calculator()
+      document.getElementById("textAreaId").setAttribute("class", "textAreaStyle_Block")
+      document.getElementById("textAreaId").blur()
+      calculator()
     }
     return () => clearInterval(interval);
-  }, [isActive, seconds]);
+  },[isActive, seconds]);
 
   return (
     <div className="App">
@@ -99,16 +96,17 @@ export default function App() {
       startGameByTyping={startGameByTyping}
       resetGame={resetGame}
       handleKeyPress={handleKeyPress}
+      level={level}
       isActive={isActive}
       seconds={seconds}
-      totalWords={totalWords}
+      totalRightWords={totalRightWords}
       paragraph={paragraph}
-      level={level}
       wrongWordsArray={wrongWordsArray}
       paragraphNum={paragraphNum}
       mistakesOn={mistakesOn}
       levelSeleted={levelSeleted}
-      check={check}
+      typeToStarOn={typeToStarOn}
+      text={text}
       />
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -5,58 +5,69 @@ import { nanoid } from 'nanoid'
 export default function Board(props) {
 
     const mistakes = props.wrongWordsArray.map(elements => <div 
-        key={nanoid()}
-        >{elements}</div>)
+    key={nanoid()}
+    >{elements}</div>)
 
     return (
-        
         <div className="gameContainer">
-            <h2
-                className="titleStyle"
-            >Typing Game</h2>
+
+            <h2 className="titleStyle">Typing Game</h2>
 
             {props.paragraphNum && <div className="paragraph">{props.paragraph[props.paragraphNum]}</div>}
             {props.paragraphNum &&<div className="length">Goal: {props.paragraph[props.paragraphNum].split(" ").length} words/min</div>}
             
+            <div>
             <textarea
                 placeholder="Press Enter when you finish to type..."
                 className="textAreaStyle_Disabled"
                 id="textAreaId"
                 name="textAreaName"
-                onChange={props.startGameByTyping}
+                value={props.text}
+                onChange={(event)=>props.startGameByTyping(event)}
                 onKeyPress={props.handleKeyPress}
             ></textarea>
-            <div
+            <div className="parpadeaTwo countdown">{props.seconds <=3 && props.seconds > 0? props.seconds : <div></div>}</div>
+            </div>
+
+
+            {props.seconds &&<div
                 className="timeStyle"
-            >Time remaining: {props.seconds} {props.seconds > 0? "second" : "seconds"}</div>
+            >Time remaining: {props.seconds} {props.seconds <= 1? "second" : "seconds"}</div>}
 
-            {props.totalWords&& <div
+            {props.totalRightWords&& 
+            <div
                 className="totalWordsStyle"
-            >Words count: {props.totalWords}</div>}
+            >Right Words Count: {props.totalRightWords}</div>}
 
-            {props.mistakesOn && props.wrongWordsArray.length > 0? 
-            <div className="mistakes">Mistakes:{mistakes}</div> : <div></div>}
+            {props.mistakesOn && props.wrongWordsArray.length > 0 && 
+            <div 
+                className="mistakes"
+            >Mistakes: {mistakes}</div>}
             
-            {!props.levelSeleted&& <button
-                className="buttonStyle"
+            {props.typeToStarOn && 
+            <button
+                className="resetStyle"
                 onClick={props.resetGame}
             >RESET</button>}
             
             {props.levelSeleted?
-            <div className="parpadea choose">Choose a level</div> 
-            : props.check? <div></div> : <div className="parpadea choose">Type to start</div>}
+            <div 
+                className="parpadea choose">Choose a level</div> 
+            : props.typeToStarOn? 
+            <div></div> 
+            : <div className="parpadea choose">Type to start</div>}
             
-            {!props.mistakesOn&& <button 
+            {!props.typeToStarOn && <button 
                 onClick={(event)=>props.level(event)}
-                value="60"
+                value="5"
                 name="0"
                 className="level">Level 1</button>}
-            {!props.mistakesOn&& <button 
+            {!props.typeToStarOn && <button 
                 onClick={(event)=>props.level(event)}
                 value="60"
                 name="1"
                 className="level">Level 2</button>}
-            {!props.mistakesOn && <button
+            {!props.typeToStarOn && <button
                 onClick={(event)=>props.level(event)} 
                 value="60"
                 name="2"
